f3ccec0c36be0da4ffbb610d3c16036552b482e4
Laura Ortiz Luque <lauraortizluque@gmail.com>
2018-10-27 21:54:53 +0200

    metodo numericos

--- src/basicoslauraortizluque/BasicosLauraOrtizLuque.java
+++ src/basicoslauraortizluque/BasicosLauraOrtizLuque.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package basicoslauraortizluque;
+package basicoslauraortizLuque;
 
 /**
  *
@@ -16,6 +16,44 @@ public class BasicosLauraOrtizLuque {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        numericos();//Llamada al método númericos
+    }
+    
+     /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        elMasPeque=127;
+        chiquitin=32767;
+        entero = 2147483647;
+        duplicaEntero=214748364888898989l;
+        grande=3.402823f;
+        doble = 9.223372036854775075555555555555555555555555555d;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor byte");
+        System.out.println(elMasPeque);
+        System.out.println("Valor short");
+        System.out.println(chiquitin);
+        System.out.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor long");
+        System.out.println(duplicaEntero);
+        System.out.println("Valor float");
+        System.out.println(grande);
+        System.out.println("Valor double");
+        System.out.println(doble);
     }
     
 }
