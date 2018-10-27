4e66b3df849d6a38d1010351d291a22c8cc8d246
Laura Ortiz Luque <lauraortizluque@gmail.com>
2018-10-27 21:53:54 +0200

    metodo logicos

--- src/basicoslauraortizluque/BasicosLauraOrtizLuque.java
+++ src/basicoslauraortizluque/BasicosLauraOrtizLuque.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package basicoslauraortizluque;
+package basicoslauraortizLuque;
 
 /**
  *
- * @author HP
+ * @author Laura Ortiz Luque
  */
 public class BasicosLauraOrtizLuque {
 
@@ -16,6 +16,26 @@ public class BasicosLauraOrtizLuque {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        logicos();
     }
     
+      public static void logicos() {
+
+        //Declaración de variables
+        Boolean primero;
+        Boolean segundo;
+
+        //Asignación de valores
+        primero = true;
+        segundo = false;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        if (primero!=segundo){
+            System.err.println("El valor de primero y el valor de segundo son distintos.");
+        }
+        else{
+            System.out.println("El valor de primero y el valor de segundo son iguales.");
+        }
+  
+    }
 }
